ba3745c7f1b840f7532b7d3c80efec698aa914ef
carlospiad <carlospiad@gmail.com>
2014-12-07 18:37:36 -0500

    Fix plot 3 and 4
    using the Date Time column

--- plot3.R
+++ plot3.R
@@ -1,3 +1,4 @@
+# Load the data into R
 
 if (file.exists("clean_data/electric_consumption.csv")){
 	data_location <- "clean_data/electric_consumption.csv"
@@ -25,9 +26,9 @@ png(filename='plots/plot3.png',width=480,height=480,units='px')
 # plot data
 lncol<-c('black','red','blue')
 lbls<-c('Sub_metering_1','Sub_metering_2','Sub_metering_3')
-plot(electric_consumption$Time,electric_consumption$Sub_metering_1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
-lines(electric_consumption$Time,electric_consumption$Sub_metering_2,col=lncol[2])
-lines(electric_consumption$Time,electric_consumption$Sub_metering_3,col=lncol[3])
+plot(electric_consumption$DateTime,electric_consumption$Sub_metering_1,type='l',col=lncol[1],xlab='',ylab='Energy sub metering')
+lines(electric_consumption$DateTime,electric_consumption$Sub_metering_2,col=lncol[2])
+lines(electric_consumption$DateTime,electric_consumption$Sub_metering_3,col=lncol[3])
 
 # add legend
 legend('topright',legend=lbls,col=lncol,lty='solid')
